7994903efd4d9c34abb1f9768b643b48a7ff31f3
Ikape Isaac <109391992+ikape@users.noreply.github.com>
2023-09-07 20:04:07 +0100

    added build

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold mb-5">Hi, Welcome</h1>
+      <p className="mb-5">
+        This is my demo app of me testing Next Js with clerk....I hope you enjoy it.
+        please check around...Go ahead and sign up or sign in
+      </p>
+    </>
+  )
+}
